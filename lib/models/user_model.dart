ea84381431ed4a01022ae0cb04f88db263ba1741
Karanbohara01 <karanbohara216@gmail.com>
2024-02-07 23:57:57 +0545

    splash screen

---
+++ lib/models/user_model.dart
@@ -0,0 +1,76 @@
+// To parse this JSON data, do
+//
+//     final userModel = userModelFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+UserModel? userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel? data) => json.encode(data!.toJson());
+
+class UserModel {
+  UserModel({
+    this.id,
+    this.userId,
+    this.name,
+    this.username,
+    this.phone,
+    this.imageUrl,
+    this.imagePath,
+    this.fcm,
+    this.email,
+    this.password,
+  });
+
+  String? id;
+  String? userId;
+  String? name;
+  String? username;
+  String? phone;
+  String? imageUrl;
+  String? imagePath;
+  String? fcm;
+  String? email;
+  String? password;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json["id"],
+    userId: json["user_id"],
+    name: json["name"],
+    username: json["username"],
+    phone: json["phone"],
+    imageUrl: json["imageUrl"],
+    imagePath: json["imagePath"],
+    fcm: json["fcm"],
+    email: json["email"],
+    password: json["password"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "user_id": userId,
+    "name": name,
+    "username": username,
+    "phone": phone,
+    "imageUrl": imageUrl,
+    "imagePath": imagePath,
+    "fcm": fcm,
+    "email": email,
+    "password": password,
+  };
+  factory UserModel.fromFirebaseSnapshot(DocumentSnapshot<Map<String, dynamic>> json) => UserModel(
+    id: json.id,
+    userId: json["user_id"],
+    name: json["name"],
+    username: json["username"],
+    phone: json["phone"],
+    imageUrl: json["imageUrl"],
+    imagePath: json["imagePath"],
+    fcm: json["fcm"],
+    email: json["email"],
+    password: json["password"],
+  );
+
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce/viewmodels/auth_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+import '../services/local_notification_service.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late AuthViewModel _authViewModel;
+
+  void checkLogin() async{
+    String? token = await FirebaseMessaging.instance.getToken();
+
+    await Future.delayed(Duration(seconds: 2));
+    // check for user detail first
+    try{
+      await _authViewModel.checkLogin(token);
+      if(_authViewModel.user==null){
+        Navigator.of(context).pushReplacementNamed("/login");
+      }else{
+        NotificationService.display(
+          title: "Welcome back",
+          body: "Hello ${_authViewModel.loggedInUser?.name},\n We have been waiting for you.",
+        );
+        Navigator.of(context).pushReplacementNamed("/dashboard");
+      }
+    }catch(e){
+      Navigator.of(context).pushReplacementNamed("/login");
+    }
+
+  }
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _authViewModel = Provider.of<AuthViewModel>(context, listen: false);
+    });
+    checkLogin();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/splash.gif"),
+              SizedBox(height: 100,),
+              Text("C-Hub", style: TextStyle(
+                  fontSize: 22
+              ),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
